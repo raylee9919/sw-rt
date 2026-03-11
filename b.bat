c4216ef30126bf6a00381d5f8e3ba4ec116173f2
unknown <raylee9919@gmail.com>
2026-03-11 21:12:42 +0900

    add readme

--- b.bat
+++ b.bat
@@ -2,6 +2,22 @@
 setlocal
 cd "%~dp0"
 
+
+
+:: Get cl.exe
+where cl >nul 2>nul
+if %errorlevel%==1 (
+    echo Looking for 'vcvars64.bat'.. Recommended to run from the Developer Command Prompt.
+    @call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+)
+
+where /q cl || (
+    echo [ERROR]: "cl" not found - please run this from the MSVC x64 native tools command prompt.
+    exit /b 1
+)
+
+
+
 for %%a in (%*) do set "%%a=1"
 
 set flags_common=/nologo /Od /Zi /W4 /wd4100 /wd4201 /wd4505 /wd4146 /wd4456 /wd4244 /EHsc- /D_CRT_SECURE_NO_WARNINGS 
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Software Raytracer (WIP WIP WIP)
+![Show](data/image1.png "image1")  
+
+
+## Build
+#### Windows (MSVC cl x64)
+Within the root directory of the project, simply run <strong>b.bat</strong>
+``` console
+> b
+```
